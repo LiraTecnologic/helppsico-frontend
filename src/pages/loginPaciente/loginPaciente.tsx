1a128b088f401f674d02413ff23fcc44855979a2
JoaoVictorNas <joacrestani07@gmail.com>
2025-03-28 19:57:52 -0300

    #FTR46:feat/ajustando resposividade e funções de validação de login

--- src/pages/loginPaciente/loginPaciente.tsx
+++ src/pages/loginPaciente/loginPaciente.tsx
@@ -4,35 +4,44 @@ import Botao from '../../components/commmon/botao/botao';
 import { useState } from 'react';
 
 const Login = () => {
-    const [email, setEmail] = useState('');
+    const [crp, setCrp] = useState('');
     const [senha, setSenha] = useState('');
 
+    const validarSenha = (senha: string) => {
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+        return regex.test(senha);
+    };
+
     const validarLogin = () => {
-        if (!email || !senha) {
+        if (!crp || !senha) {
             alert('Por favor, preencha todos os campos antes de continuar.');
             return;
         }
-        
+
+        if (!validarSenha(senha)) {
+            alert('A senha deve ter no mínimo 6 caracteres, incluindo uma letra maiúscula, uma letra minúscula, um número e um caractere especial.');
+            return;
+        }
     };
 
     return (
-        <div className="loginPaciente-container">
-            <img src="../../src/assets/marcaDagua-login.png" alt="marca d'água" className="marcaDagua-loginPaciente" />
+        <div className="login-container-paciente">
+            <img src="../../src/assets/marcaDagua-login.png" alt="marca d'água" className="nova-marca-dagua" />
             
-            <div className="left-contentPaciente">
-                <div className='formularioPaciente'>
-                    <h1 className='text-white'>Minha Conta</h1>
-                    <InputTotal label="E-mail:" pleaceHolder="Digite seu e-mail..." tipo="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <div className="left-content-paciente">
+                <div className='formulario-paciente'>
+                    <h1 className='texto-titulo-paciente'>Minha Conta</h1>
+                    <InputTotal label="E-mail:" pleaceHolder="Digite seu e-mail..." tipo="text" value={crp} onChange={(e) => setCrp(e.target.value)} />
                     <InputTotal label="Senha:" pleaceHolder="Digite sua senha..." tipo="password" value={senha} onChange={(e) => setSenha(e.target.value)} />
-                    <p className='semConta'>Não tem conta? <a href="/" className='link'>crie agora</a></p>
+                    <p className='novo-texto-sem-conta'>Não tem conta? <a href="" className='link-paciente'>crie agora</a></p>
                     <Botao texto='Entrar' onClick={validarLogin} />
                 </div>
             </div>
 
-            <div className="linha-vertical"></div>
+            <div className="nova-linha-vertical"></div>
 
-            <div className="right-contentPaciente">
-                <img src="../../src/assets/image-loginPaciente.svg" alt="login psicologo" className='imagePaciente'/>
+            <div className="right-content-paciente">
+                <img src="../../src/assets/image-loginPaciente.svg" alt="login paciente" className='imagem-login-paciente'/>
             </div>
         </div>
     );
