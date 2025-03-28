84d6d00f5fe3dde61eb4032c3499acce1a68f398
JoaoVictorNas <joacrestani07@gmail.com>
2025-03-27 21:29:33 -0300

    #FRT46:feat/Finalizando verificaçãode login e ajustes nos inputs e botão

--- src/components/commmon/Inputs/InputTotal.tsx
+++ src/components/commmon/Inputs/InputTotal.tsx
@@ -3,13 +3,22 @@ import './InputTotal.css';
 interface InputTotalProps {
     pleaceHolder: string;
     label: string;
+    tipo: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function InputTotal(props: InputTotalProps) {
+function InputTotal({ pleaceHolder, label, tipo, value, onChange }: InputTotalProps) {
     return (
         <div className='contentFullInput'>
-            <label className='textLabel'>{props.label}</label>
-            <input type="text" placeholder={props.pleaceHolder} className='inputFull'/>
+            <label className='textLabel'>{label}</label>
+            <input 
+                type={tipo} 
+                placeholder={pleaceHolder} 
+                className='inputFull' 
+                value={value}
+                onChange={onChange}
+            />
         </div>
     );
 }
--- src/pages/loginPaciente/loginPaciente.tsx
+++ src/pages/loginPaciente/loginPaciente.tsx
@@ -1,19 +1,31 @@
 import './loginPaciente.css';
 import InputTotal from '../../components/commmon/Inputs/InputTotal';
 import Botao from '../../components/commmon/botao/botao';
+import { useState } from 'react';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const validarLogin = () => {
+        if (!email || !senha) {
+            alert('Por favor, preencha todos os campos antes de continuar.');
+            return;
+        }
+        
+    };
+
     return (
         <div className="loginPaciente-container">
-            <img src="../../src/assets/marcaDagua-login.png" alt="marca d'água" className="marca-dagua-1" />
+            <img src="../../src/assets/marcaDagua-login.png" alt="marca d'água" className="marcaDagua-loginPaciente" />
             
             <div className="left-contentPaciente">
                 <div className='formularioPaciente'>
                     <h1 className='text-white'>Minha Conta</h1>
-                    <InputTotal label="E-mail:" pleaceHolder="Digite seu e-mail..." />
-                    <InputTotal label="Senha:" pleaceHolder="Digite sua senha..." />
+                    <InputTotal label="E-mail:" pleaceHolder="Digite seu e-mail..." tipo="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <InputTotal label="Senha:" pleaceHolder="Digite sua senha..." tipo="password" value={senha} onChange={(e) => setSenha(e.target.value)} />
                     <p className='semConta'>Não tem conta? <a href="/" className='link'>crie agora</a></p>
-                    <Botao texto='Entrar' />
+                    <Botao texto='Entrar' onClick={validarLogin} />
                 </div>
             </div>
 
