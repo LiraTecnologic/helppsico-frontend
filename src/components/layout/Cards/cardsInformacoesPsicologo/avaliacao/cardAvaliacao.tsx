389b55298dbee3608e1cc2d050da10951917ef41
VitorVieira <vitorhvvieira@gmail.com>
2025-05-08 16:04:20 -0300

    #FTR19:feat/Finalizado tela com 90% de fidelidade ao protótipo

--- src/components/layout/Cards/cardsInformacoesPsicologo/avaliacao/cardAvaliacao.tsx
+++ src/components/layout/Cards/cardsInformacoesPsicologo/avaliacao/cardAvaliacao.tsx
@@ -9,13 +9,23 @@ interface Avaliacao {
 }
 
 export default function CardAvaliacao(avaliacao: Avaliacao) {
+    
+    function formatarData(dataISO: string): string {
+        const data = new Date(dataISO);
+        return new Intl.DateTimeFormat('pt-BR', {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric'
+        }).format(data);
+    }
+
     return (
         <div className="card-avaliacao-listagem">
             <div className="card-avaliacao-info">
                 <img src={avaliacao.fotoPaciente} alt="Foto psicologo" />
-                <div>
-                    <p>{avaliacao.nomePaciente}</p>
-                    <p>{avaliacao.data}</p>
+                <div className="nome-data-avaliacao">
+                    <p className="nome-paciente">{avaliacao.nomePaciente}</p>
+                    <p className="data-avaliacao">{formatarData(avaliacao.data)}</p>
                 </div>
                 <div className="estrelas-card-avaliacao">
                     <img src={Estrela} alt="Estrela" />
@@ -25,7 +35,7 @@ export default function CardAvaliacao(avaliacao: Avaliacao) {
                     <img src={Estrela} alt="Estrela" />
                 </div>
             </div>
-            <p>{avaliacao.conteudo}</p>
+            <p className="conteudo-avaliacao">{avaliacao.conteudo}</p>
         </div>
     );
 }
