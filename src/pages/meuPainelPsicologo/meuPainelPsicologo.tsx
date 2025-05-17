13fd816975cc92d6fbde4de1a0e2e1acc01e71ed
VitorVieira <vitorhvvieira@gmail.com>
2025-05-17 00:36:23 -0300

    #FTR37:feat/Começando a integração do meu painel de psicologo

--- src/pages/meuPainelPsicologo/meuPainelPsicologo.tsx
+++ src/pages/meuPainelPsicologo/meuPainelPsicologo.tsx
@@ -3,38 +3,55 @@ import Header from '../../components/layout/header/header';
 import ProximasSessoes from '../../components/layout/Cards/proximaSessao/proximaSessao';
 import ListagemDocumentos from '../../components/layout/Cards/listagemDeDocumentos/listagemDocumentos';
 import ListagemDePacientes from '../../components/layout/listagemDePacientes/listagemDePacientes';
+import { useState, useEffect } from 'react';
+import ConsultaModel from '../../models/consulta';
+import VinculoModel from '../../models/vinculo';
+import ProntuarioModel from '../../models/prontuario';
+import { consultaProntuarios } from "../listagemProntuario/listagemProntuario.service";
 
-export default function MeuPainelPsicologo(){
-    const documentos = [
-        { id: '1', titulo: 'Documento bonito' },
-        { id: '2', titulo: 'Relatório de Sessão' },
-        { id: '3', titulo: 'Plano de Tratamento' },
-        { id: '4', titulo: 'Andre feioso' },
-        { id: '5', titulo: 'Andre feioso' }
-    ];
+export default function MeuPainelPsicologo() {
 
-    const pacientes = [
-        { fotoUrl: 'https://forbes.com.br/wp-content/uploads/2021/06/ForbesTech_Lucas-Carrilho-Pessoa-group-project-manager-do-iFood_Divulgacao.png', nome: 'João Victor' },
-        { fotoUrl: 'https://forbes.com.br/wp-content/uploads/2021/06/ForbesTech_Lucas-Carrilho-Pessoa-group-project-manager-do-iFood_Divulgacao.png', nome: 'Maria Souza' },
-        { fotoUrl: 'https://forbes.com.br/wp-content/uploads/2021/06/ForbesTech_Lucas-Carrilho-Pessoa-group-project-manager-do-iFood_Divulgacao.png', nome: 'Carlos Lima' }
-      ];
+    const [consultas, setConsutas] = useState<ConsultaModel[]>([]);
+    const [vinculos, setVinculos] = useState<VinculoModel[]>([]);
+    const [prontuarios, setProntuarios] = useState<ProntuarioModel[]>([]);
+
+    useEffect(() => {
+        async function carregarProntuarios() {
+            const prontuariosConsultados = await consultaProntuarios();
+            setProntuarios(prontuariosConsultados);
+        }
+
+        async function carregarConsultasFuturas() {
+            const consultasConsultadas = await consultarConsultas();
+            setConsutas(consultasConsultadas);
+        }
+
+        async function carregarVinculos() {
+            const vinculosConsultados = await consultarVinculos();
+            setVinculos(vinculosConsultados);
+        }
+
+        carregarProntuarios();
+        carregarConsultasFuturas();
+        carregarVinculos();
+    }, []);
 
     const handleDocumentoClick = (id: string) => {
         console.log(`Documento com ID ${id} foi clicado`);
         alert("clicou no documento");
     };
 
-    return(
+    return (
         <>
-            <Header fluxo='meuPainel' />
+            <Header fluxo='meuPainel' headerPsicologo={true} />
             <ProximasSessoes sessaoMarcada={false} verMais={true} fluxo='psicologo' />
             <div className="imagensPacientes">
                 <ListagemDePacientes pacientes={pacientes} verMais={true} />
             </div>
             <h1 className='prontuarioTittle'>Prontuário</h1>
-            <ListagemDocumentos 
-                documentos={documentos} 
-                onDocumentoClick={handleDocumentoClick} 
+            <ListagemDocumentos
+                documentos={documentos}
+                onDocumentoClick={handleDocumentoClick}
             />
         </>
     )
