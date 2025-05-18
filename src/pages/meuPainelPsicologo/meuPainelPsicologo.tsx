c3fa831ed92782cf81f87a0d109910882cfc43c8
VitorVieira <vitorhvvieira@gmail.com>
2025-05-18 15:39:24 -0300

    #FTR37:feat/Finalizando integração do meu painel de psicólogo

--- src/pages/meuPainelPsicologo/meuPainelPsicologo.tsx
+++ src/pages/meuPainelPsicologo/meuPainelPsicologo.tsx
@@ -17,34 +17,58 @@ export default function MeuPainelPsicologo() {
     const [consultas, setConsutas] = useState<ConsultaModel[]>([]);
     const [vinculos, setVinculos] = useState<VinculoModel[]>([]);
     const [prontuarios, setProntuarios] = useState<ProntuarioModel[]>([]);
-    const [psicologoLogado, setPsicologoLogado] = useState<PsicologoModel>();
 
     useEffect(() => {
         async function carregarProntuarios(id: string) {
             const prontuariosConsultados = await consultaProntuariosPsicologo(id, 1);
+            console.log('Prontuarios: ', prontuariosConsultados);
             setProntuarios(prontuariosConsultados.content);
         }
 
-        async function carregarConsultasFuturas(id: string) {
+        async function carregarConsultas(id: string) {
             const consultasConsultadas = await consultaSessoesFuturas(id, 1);
-            setConsutas(consultasConsultadas.content);
+            console.log('Consultas: ', consultasConsultadas);
+
+            const consultasOrdenadas = consultasConsultadas.content.sort((a, b) => {
+                const dataA = new Date(a.dataHora).getTime();
+                const dataB = new Date(b.dataHora).getTime();
+                const agora = Date.now();
+
+                const diffA = Math.abs(dataA - agora);
+                const diffB = Math.abs(dataB - agora);
+
+                return diffA - diffB;
+            });
+
+            setConsutas(consultasOrdenadas);
         }
 
         async function carregarVinculos(id: string) {
             const vinculosConsultados = await consultaVinculosPsicologo(id, 1);
+            console.log('Vinculos: ', vinculosConsultados);
             setVinculos(vinculosConsultados.content);
         }
 
-        const psicologoString = localStorage.getItem('psicologoLogado');
-        if (psicologoString) {
-            const psicologo: PsicologoModel = JSON.parse(psicologoString);
-            setPsicologoLogado(psicologo);
 
-            carregarConsultasFuturas(psicologo.id);
-            carregarProntuarios(psicologo.id);
-            carregarVinculos(psicologo.id);
-        }
+        // // Apenas nessa task para funcionar integração
+        // const idRandomPsico = '71ff60f6-0272-41db-89fb-621c488b8642';
+        // localStorage.setItem('psicologoLogado', idRandomPsico);
+
+        // const psicologoString: string = localStorage.getItem('psicologoLogado');
+
+        // const psicologoId: string = JSON.parse(psicologoString);
+
+        const psicologoId = '71ff60f6-0272-41db-89fb-621c488b8642';
+
+        carregarConsultas(psicologoId);
+        carregarProntuarios(psicologoId);
+        carregarVinculos(psicologoId);
+
     }, []);
+
+
+
+    const sessaoMarcada = consultas.length > 0;;
 
     const handleDocumentoClick = (id: string) => {
         console.log(`Documento com ID ${id} foi clicado`);
@@ -54,13 +78,20 @@ export default function MeuPainelPsicologo() {
     return (
         <>
             <Header fluxo='meuPainel' headerPsicologo={true} />
-            <ProximasSessoes sessaoMarcada={false} verMais={true} fluxo='psicologo' />
+            {consultas.length > 0 && (
+                <ProximasSessoes
+                    sessaoMarcada={true}
+                    verMais={true}
+                    fluxo='psicologo'
+                    consulta={consultas[0]}
+                />
+            )}
             <div className="imagensPacientes">
-                <ListagemDePacientes pacientes={pacientes} verMais={true} />
+                <ListagemDePacientes vinculos={vinculos} verMais={true} />
             </div>
             <h1 className='prontuarioTittle'>Prontuário</h1>
             <ListagemDocumentos
-                documentos={documentos}
+                documentos={prontuarios}
                 onDocumentoClick={handleDocumentoClick}
             />
         </>
