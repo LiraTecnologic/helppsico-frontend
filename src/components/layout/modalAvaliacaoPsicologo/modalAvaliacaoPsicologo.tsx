c9985c1442f8d73f3ae049e0e1b533491108c998
André Gustavo <andregustavodeoliveira49@gmail.com>
2025-05-27 21:06:20 -0300

    #FTR72:feat/Adicionando a estrutura base e entendendo melhor como vou realizar

---
+++ src/components/layout/modalAvaliacaoPsicologo/modalAvaliacaoPsicologo.tsx
@@ -0,0 +1,15 @@
+import './modalAvaliacaoPsicologo.css';
+
+export default function modalAvaliacaoPsicologo(){
+
+
+    return(
+        <div>
+            <h2>Avaliar Psicólogo:</h2>
+            {/* Foto e nome do meliante */}
+            {/* Mostra 5 estrelas clicaveis - 1, 2, 3, 4 e 5 */}
+            <p>Comentário:</p>
+            <input></input>
+        </div>
+    )
+}
