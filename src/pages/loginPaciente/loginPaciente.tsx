bdd1d79f12d82c4202e82af60cd167a2f1da7333
RAULALBERT001 <albertiraulsergio@gmail.com>
2025-06-04 20:53:13 -0300

    #FTR91:feat/ integrando o auth service com a tela de Login do paciente

--- src/pages/loginPaciente/loginPaciente.tsx
+++ src/pages/loginPaciente/loginPaciente.tsx
@@ -4,51 +4,101 @@ import Botao from '../../components/commmon/botoes/botao/botao';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
+
+import { login } from '../../services/auth.service';
+
 const Login = () => {
-    const [crp, setCrp] = useState('');
-    const [senha, setSenha] = useState('');
-    const navigate = useNavigate();
+  const [email, setEmail] = useState('');  
+  const [senha, setSenha] = useState('');
+  const navigate = useNavigate();
 
-    const validarSenha = (senha: string) => {
-        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
-        return regex.test(senha);
-    };
+  const validarSenha = (senha: string) => {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+    return regex.test(senha);
+  };
 
-    const validarLogin = () => {
-        if (!crp || !senha) {
-            alert('Por favor, preencha todos os campos antes de continuar.');
-            return;
-        }
 
-        if (!validarSenha(senha)) {
-            alert('A senha deve ter no mínimo 6 caracteres, incluindo uma letra maiúscula, uma letra minúscula, um número e um caractere especial.');
-            return;
-        }
+  const validarLogin = async () => {
+    if (!email || !senha) {
+      alert('Por favor, preencha todos os campos antes de continuar.');
+      return;
+    }
 
-        return(navigate("/paciente/painel"))
-    };
+    if (!validarSenha(senha)) {
+      alert(
+        'A senha deve ter no mínimo 6 caracteres, incluindo uma letra maiúscula, uma letra minúscula, um número e um caractere especial.'
+      );
+      return;
+    }
 
-    return (
-        <div className="login-container-paciente">
-            <img src="../../src/assets/marcaDagua-login.png" alt="marca d'água" className="nova-marca-dagua" />
-            
-            <div className="left-content-paciente">
-                <div className='formulario-paciente'>
-                    <h1 className='texto-titulo-paciente'>Minha Conta</h1>
-                    <InputTotal label="E-mail:" pleaceHolder="Digite seu e-mail..." tipo="text" value={crp} onChange={(e) => setCrp(e.target.value)} />
-                    <InputTotal label="Senha:" pleaceHolder="Digite sua senha..." tipo="password" value={senha} onChange={(e) => setSenha(e.target.value)} />
-                    <p className='novo-texto-sem-conta'>Não tem conta? <Link to="/cadastroPaciente" className='link-paciente'>crie agora</Link></p>
-                    <Botao texto='Entrar' onClick={validarLogin} />
-                </div>
-            </div>
+    try {
+    
+      const resposta = await login(email, senha, 'PACIENTE');
 
-            <div className="nova-linha-vertical"></div>
+      if (resposta.dado) {
+      
+        navigate('/paciente/painel');
+      } else {
+       
+        alert(`Falha na autenticação: ${resposta.erro}`);
+      }
+    } catch (err) {
+     
+      alert(`Ocorreu um erro ao tentar logar: ${err}`);
+    }
+  };
 
-            <div className="right-content-paciente">
-                <img src="../../src/assets/image-loginPaciente.svg" alt="login paciente" className='imagem-login-paciente'/>
-            </div>
+  return (
+    <div className="login-container-paciente">
+      <img
+        src="../../src/assets/marcaDagua-login.png"
+        alt="marca d'água"
+        className="nova-marca-dagua"
+      />
+
+      <div className="left-content-paciente">
+        <div className="formulario-paciente">
+          <h1 className="texto-titulo-paciente">Minha Conta</h1>
+
+
+          <InputTotal
+            label="E-mail:"
+            pleaceHolder="Digite seu e-mail..."
+            tipo="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <InputTotal
+            label="Senha:"
+            pleaceHolder="Digite sua senha..."
+            tipo="password"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
+
+          <p className="novo-texto-sem-conta">
+            Não tem conta?{' '}
+            <Link to="/cadastroPaciente" className="link-paciente">
+              crie agora
+            </Link>
+          </p>
+
+  
+          <Botao texto="Entrar" onClick={validarLogin} />
         </div>
-    );
-}
+      </div>
+
+      <div className="nova-linha-vertical"></div>
+
+      <div className="right-content-paciente">
+        <img
+          src="../../src/assets/image-loginPaciente.svg"
+          alt="login paciente"
+          className="imagem-login-paciente"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Login;
